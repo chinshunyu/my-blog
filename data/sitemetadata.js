9da2701d2ab221ccba2bc7fb25d624f179bb14ec
chin shunyu <johncachyjp@gmail.com>
2024-09-16 18:01:37 +0800

    修改主页

--- data/sitemetadata.js
+++ data/sitemetadata.js
@@ -1,22 +1,49 @@
+// const siteMetadata = {
+//   title: "Prologue 序章",
+//   author: "槐序",
+//   authorDesc:"自我批判和反叛思考",
+//   publishName: "Prologue",
+//   headerTitle: "What's past is prologue",
+//   description:
+//     "对当下的反思和批判",
+//   language: "zh-CN",
+//   keywords:['prologue', '序章'],
+//   siteUrl: "https://prologue.dev",
+//   siteRepo: "prologue.dev",
+//   repoid:"R_kgDOHl4HGg",
+//   categoryid: "DIC_kwDOHl4HGs4CP_Jc",
+//   siteLogo: "/static/favicons/favicon.ico",
+//   avatar: "/static/favicons/avatar2-square.jpg",
+//   cover: "/static/favicons/cover.png",
+//   email: "author@prologue.dev",
+//   github: "moreanlog",
+//   twitter: "@huaixuOvO",
+//   twitterid:"1507965040603074564",
+// };
+
+// module.exports = siteMetadata;
+
+
+
 const siteMetadata = {
-  title: "Prologue 序章",
-  author: "槐序",
+  title: "JohnCachy's blog",
+  author: "JohnCachy",
   authorDesc:"自我批判和反叛思考",
-  publishName: "Prologue",
+  publishName: "JohnCachy",
   headerTitle: "What's past is prologue",
   description:
     "对当下的反思和批判",
   language: "zh-CN",
   keywords:['prologue', '序章'],
-  siteUrl: "https://prologue.dev",
-  siteRepo: "prologue.dev",
-  repoid:"R_kgDOHl4HGg",
-  categoryid: "DIC_kwDOHl4HGs4CP_Jc",
+  siteUrl: "https://johncachy.us.kg/",
+  siteRepo: "my-blog",
+  repoid:"R_kgDOMsx20A",
+  categoryid: "IC_kwDOMsx20M4CigkS",
   siteLogo: "/static/favicons/favicon.ico",
-  avatar: "/static/favicons/avatar2-square.jpg",
+  avatar: "/static/favicons/avatar.jpeg",
   cover: "/static/favicons/cover.png",
-  email: "author@prologue.dev",
-  github: "moreanlog",
+  email: "johncachyjp@gmail.com",
+  github: "chinshunyu",
   twitter: "@huaixuOvO",
   twitterid:"1507965040603074564",
 };
